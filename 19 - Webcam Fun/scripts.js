28d12f0eb796adb6e0d12c0d8c2f710778fb81f5
Nader Akhgari <nader90akhgari@gmail.com>
2020-05-11 00:50:50 +0100

    got the media device in this project

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,19 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);      
+      video.srcObject = localMediaStream;
+      console.log(video.videoWidth)
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+getVideo();
+
